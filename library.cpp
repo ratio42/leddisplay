832ef450d87eb88e639648acb589e9688948ef8c
ratio <peter@pethor.de>
2022-11-28 20:18:23 +0100

    added initialization and tear down routines for SDL2 graphical output window and added calls in Connect and Disconnect

--- library.cpp
+++ library.cpp
@@ -1,17 +1,50 @@
 #include "library.h"
 #include "internal.h"
 
+#include <SDL.h>
+
 #include <iostream>
 #include <thread>
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // global objects
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 LibraryState g_LibraryState;
 Display g_Display(64, 32);
 std::unique_ptr<std::thread> g_CyclicLoop;
 bool g_StopDisplayLoop = false;
 
-constexpr int loopCycleInMs = 50; // 50ms is 20 fps
+constexpr int c_LoopCycleInMs = 50; // 50ms is 20 fps
+constexpr int c_WindowWidth = 500;
+constexpr int c_WindowHeight = 300;
 
+// SDL objects
+SDL_Renderer *g_SdlRenderer = nullptr; // pointer for the renderer
+SDL_Window *g_SdlWindow = nullptr; // pointer for the window
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// graphical output and drawing routines
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// TODO: move SDL part (or probably all drawing) into separate file
+
+void InitGraphicalOutput() {
+    SDL_Init(SDL_INIT_VIDEO);       // Initializing SDL as Video
+    SDL_CreateWindowAndRenderer(c_WindowWidth, c_WindowHeight, 0, &g_SdlWindow, &g_SdlRenderer);
+    SDL_SetRenderDrawColor(g_SdlRenderer, 0, 0, 0, 0);      // setting draw color
+    SDL_RenderClear(g_SdlRenderer);      // Clear the newly created window
+    SDL_RenderPresent(g_SdlRenderer);    // Reflects the changes done in the window.
+}
+
+void StopGraphicalOutput() {
+    SDL_DestroyRenderer(g_SdlRenderer);
+    SDL_DestroyWindow(g_SdlWindow);
+    SDL_Quit();
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// (static) helper routines
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 static void DebugWrite(const std::string& debugText_p)
 {
     if (g_LibraryState.IsDebugOutputEnabled()) {
@@ -20,7 +53,7 @@ static void DebugWrite(const std::string& debugText_p)
 }
 
 void CyclicLoop() {
-    const auto timeWindow = std::chrono::milliseconds(loopCycleInMs);
+    const auto timeWindow = std::chrono::milliseconds(c_LoopCycleInMs);
 
     while(!g_StopDisplayLoop)
     {
@@ -33,6 +66,9 @@ void CyclicLoop() {
     }
 }
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// implementation of API routines
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void Connect(bool enableDebugOutput) {
     if (enableDebugOutput)
     {
@@ -40,6 +76,9 @@ void Connect(bool enableDebugOutput) {
     }
     g_LibraryState.SetConnected(true);
     DebugWrite("Display connected!");
+
+    // initalize graphic output
+    InitGraphicalOutput();
 
     // start cyclic loop, for write access to display and refreshing graphical output
     g_CyclicLoop = std::make_unique<std::thread>(CyclicLoop);
@@ -60,10 +99,13 @@ bool IsConnected() {
 
 void Disconnect() {
     DebugWrite("Going to disconnect!");
+
     if (g_CyclicLoop) {
         g_StopDisplayLoop = true;
         g_CyclicLoop->join();
     }
+    StopGraphicalOutput();
+
     g_StopDisplayLoop = false;
     g_LibraryState.SetConnected(false);
 }
