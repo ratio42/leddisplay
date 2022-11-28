f8ece10dbf84d2e0953649ce19303b4250964167
ratio <peter@pethor.de>
2022-11-29 00:37:06 +0100

    renamed, improved and added new unit tests (real turn on of LEDs and getting state)

--- UnitTest/unittest.cpp
+++ UnitTest/unittest.cpp
@@ -11,10 +11,10 @@ TEST(ConnectionTest, WithoutConnectingStateIsNotConnected)
     ASSERT_FALSE(isConnected);
 }
 
-TEST(ConnectionTest, AfterConnectingWithoutDebugStateIsConnected)
+TEST(ConnectionTest, AfterConnectingWithoutDebugAndGraphicalOutputStateIsConnected)
 {
     // arrange & act
-    Connect(false);
+    Connect();
     bool isConnected{IsConnected()};
     Disconnect();
 
@@ -22,10 +22,28 @@ TEST(ConnectionTest, AfterConnectingWithoutDebugStateIsConnected)
     ASSERT_TRUE(isConnected);
 }
 
-TEST(ConnectionTest, AfterConnectingWithDebugStateIsConnected)
+TEST(ConnectionTest, AfterConnectingWithDebugAndWithoutGraphicalOutputStateIsConnected)
 {
-    // arrange & act
-    Connect(true);
+    // arrange
+    const bool debugState{true};
+
+    // act
+    Connect(debugState);
+    bool isConnected{IsConnected()};
+    Disconnect();
+
+    // assert
+    ASSERT_TRUE(isConnected);
+}
+
+TEST(ConnectionTest, AfterConnectingWithDebugAndGraphicalOutputStateIsConnected)
+{
+    // arrange
+    const bool debugState{true};
+    const bool graphicalOutputState{true};
+
+    // act
+    Connect(debugState, graphicalOutputState);
     bool isConnected{IsConnected()};
     Disconnect();
 
@@ -55,18 +73,98 @@ public:
 };
 
 void LedStatusTests::SetUp() {
-    // connect with debug output
-    Connect(true);
+    // connect with debug output and graphical output
+    Connect(true, true);
 }
 
 void LedStatusTests::TearDown() {
+    // TODO: seams like leds keep turned on after disconnect --> at least connect should turn all of
     Disconnect();
 }
 
-TEST_F(LedStatusTests, Test1) {
-    sleep(1);
+TEST_F(LedStatusTests, TurnSingleLedInUlRedOn) {
+    // arrange
+    int x{0}, y{0}, r{100}, g{0}, b{0};
+
+    // act
+    const bool ledIsOnBeforeTurningOn{LedIsOn(x, y)};
+    LedOn(x, y, r, g, b);
+    const bool ledIsOnAfterTurningOn{LedIsOn(x, y)};
+
+    // assert
+    ASSERT_FALSE(ledIsOnBeforeTurningOn);
+    ASSERT_TRUE(ledIsOnAfterTurningOn);
 }
 
+TEST_F(LedStatusTests, TurnSingleLedInUrRedOn) {
+    // arrange
+    int x{63}, y{0}, r{100}, g{0}, b{0};
+
+    // act
+    const bool ledIsOnBeforeTurningOn{LedIsOn(x, y)};
+    LedOn(x, y, r, g, b);
+    const bool ledIsOnAfterTurningOn{LedIsOn(x, y)};
+
+    // assert
+    ASSERT_FALSE(ledIsOnBeforeTurningOn);
+    ASSERT_TRUE(ledIsOnAfterTurningOn);
+}
+
+TEST_F(LedStatusTests, TurnSingleLedInLlRedOn) {
+    // arrange
+    int x{0}, y{31}, r{100}, g{0}, b{0};
+
+    // act
+    const bool ledIsOnBeforeTurningOn{LedIsOn(x, y)};
+    LedOn(x, y, r, g, b);
+    const bool ledIsOnAfterTurningOn{LedIsOn(x, y)};
+
+    // assert
+    ASSERT_FALSE(ledIsOnBeforeTurningOn);
+    ASSERT_TRUE(ledIsOnAfterTurningOn);
+}
+
+TEST_F(LedStatusTests, TurnSingleLedInLrRedOn) {
+    // arrange
+    int x{63}, y{31}, r{100}, g{0}, b{0};
+
+    // act
+    const bool ledIsOnBeforeTurningOn{LedIsOn(x, y)};
+    LedOn(x, y, r, g, b);
+    const bool ledIsOnAfterTurningOn{LedIsOn(x, y)};
+
+    // assert
+    ASSERT_FALSE(ledIsOnBeforeTurningOn);
+    ASSERT_TRUE(ledIsOnAfterTurningOn);
+}
+
+TEST_F(LedStatusTests, TurnEveryLedOnWithColorRamp) {
+    // arrange
+    int rampMinRed{30}, rampMaxRed{250}, rampStepRed{10};
+    int rampMinGreen{0}, rampMaxGreen{250}, rampStepGreen{15};
+    int rampMinBlue{50}, rampMaxBlue{250}, rampStepBlue{5};
+
+    // act
+    int red{rampMinRed};
+    int green{rampMinGreen};
+    int blue{rampMinBlue};
+    for (int x=0; x < 64; x++) {
+        for (int y=0; y < 32; y++) {
+            LedOn(x, y, red, green, blue);
+            red += rampStepRed;
+            green += rampStepGreen;
+            blue += rampStepBlue;
+
+            if (red > rampMaxRed) { red = rampMinRed; }
+            if (green > rampMaxGreen) { green = rampMinGreen; }
+            if (blue > rampMaxBlue) { blue= rampMinBlue; }
+        }
+    }
+    const bool ledIsOnAfterTurningOn{LedIsOn(17, 5)};
+
+    // assert - just check on random led
+    ASSERT_TRUE(ledIsOnAfterTurningOn);
+}
 
 
 int main(int argc, char *argv[])
